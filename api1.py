a5d856f558c1b2f0b4749eb00934a860f7bcc138
Ra <ravicity999@gmail.com>
2023-04-16 02:25:43 +0530

    fixed a lot of issues

--- api1.py
+++ api1.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from node import RaftNode
 import asyncio
 import sys
@@ -9,7 +9,10 @@ api = Flask(__name__)
 nodes = {
 	"node1": {"ip": "127.0.0.1", "port": 8000},
 	"node2": {"ip": "127.0.0.1", "port": 8001},
+	"node3": {"ip": "127.0.0.1", "port": 8002}
+	
 }
+
 
 # # Create and start the nodes
 # tasks = []
@@ -25,33 +28,47 @@ raft_node = RaftNode(node_id, nodes)
 #raft_node.state = "candidate"
 
 @api.route("/requestVote", methods = ["GET", "POST"])
-def request_votes():
-	if request.method == "POST":
-		data= {
-		"term": raft_node.current_term,
-		"voteGranted": True
-		}
-		return data
-	#data = (raft_node.request_vote())
-	#print(data, file=sys.stderr)
-	#voting_data = data[0]
-	#response = data[1]
-	return 200
+def request_vote():
+	try:
+		if request.method == "POST":
+			data= {
+			"term": raft_node.current_term,
+			"voteGranted": True
+			}
+			term = request.get_json().get('term')
+			if term < raft_node.current_term:
+			# self.current_term = response_data['term']
+			# self.state = 'follower'
+				data["voteGranted"] = False
+			print(f"Received Heartbeat: {data}", file=sys.stdout)
+			return jsonify(data)
+		#data = (raft_node.request_vote())
+		#print(data, file=sys.stderr)
+		#voting_data = data[0]
+		#response = data[1]
+	except Exception as e:
+		print(9999999999999999999999)
+		print(e, file=sys.stderr)
+	return make_response("Success", 200)
 	
-@api.route("/apiendEntries", methods = ["POST", "GET"])
-def apiendEntries():
+@api.route("/appendEntries", methods = ["POST", "GET"])
+def appendEntries():
 	if request.method == "POST":
+		
 		data = {
 		"term": raft_node.current_term,
 		"success": True
 		}
+		print(f"Received HeartBeat {data}")
 		raft_node.reset_timeout()
-		return data
+		raft_node.timeout = 7
+		print(f"raft_node.timeout: {raft_node.timeout}")
+		return jsonify(data)
 	#data = raft_node.send_heartbeats()
 	#print(data, file=sys.stderr)
 	#entry = data[0]
 	#response = data[1]
-	return 200
+	return make_response("Success", 200)
 
 async def main():
 	# Define the nodes
