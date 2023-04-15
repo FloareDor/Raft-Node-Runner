921603fd3c6367b2b4db0e40b2cde0d8b9307775
Ra <ravicity999@gmail.com>
2023-04-15 22:00:16 +0530

    Init commit

---
+++ api1.py
@@ -0,0 +1,67 @@
+from flask import Flask, request
+from node import RaftNode
+import asyncio
+import sys
+import jsonify
+import threading
+
+api = Flask(__name__)
+
+nodes = {
+	"node1": {"ip": "127.0.0.1", "port": 8000},
+	"node2": {"ip": "127.0.0.1", "port": 8001},
+}
+
+# # Create and start the nodes
+# tasks = []
+# for node_id in nodes:
+# 	node = RaftNode(node_id, nodes)
+# 	tasks.apiend(asyncio.create_task(node.run()))
+
+# # Run the nodes' event loops
+# await asyncio.gather(*tasks)
+node_id = "node1"
+#api.run(nodes[node_id]["ip"], port=nodes[node_id]["port"])
+raft_node = RaftNode(node_id, nodes)
+
+@api.route("/requestVote", methods = ["GET", "POST"])
+def request_votes():
+	if request.method == "POST":
+		data= {
+		"term": raft_node.current_term,
+		"voteGranted": True
+		}
+		return data
+	data = (raft_node.request_vote())
+	print(data, file=sys.stderr)
+	#voting_data = data[0]
+	response = data[1]
+	return jsonify(response)
+	
+@api.route("/apiendEntries", methods = ["POST", "GET"])
+def apiendEntries():
+	if request.method == "POST":
+		data = {
+		"term": raft_node.current_term,
+		"success": True
+		}
+		return data
+	data = raft_node.send_heartbeats()
+	print(data, file=sys.stderr)
+	#entry = data[0]
+	response = data[1]
+	return jsonify(response)
+
+async def main():
+	# Define the nodes
+	await raft_node.run()
+	
+def run_flask():
+	api.run(nodes[node_id]["ip"], port=nodes[node_id]["port"])
+
+if __name__ == '__main__':
+	flask_Thread = threading.Thread(target=run_flask)
+	flask_Thread.start()
+
+	while True:
+		asyncio.run(main())
